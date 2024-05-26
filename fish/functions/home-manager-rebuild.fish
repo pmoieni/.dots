b098fce1170d185711d6e9ccfb7e5b640c67e987
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2024-05-26 19:35:54 +0330

    fix

--- fish/functions/home-manager-rebuild.fish
+++
@@ -1,5 +0,0 @@
-function home-manager-rebuild -d "rebuild home manager config"
-    pushd $HOME/.dotfiles/nix
-    time home-manager switch --flake .#pmoieni@nixos
-    popd
-end
--- fish/functions/nix-rebuild.fish
+++
@@ -1,5 +0,0 @@
-function nix-rebuild -d "rebuild nix config"
-    pushd $HOME/.dotfiles/nix
-    time sudo nixos-rebuild switch --flake .#nixos
-    popd
-end
