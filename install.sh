a06ce25f189fa373a1edb6aa3751c1295fdf12d0
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2024-09-04 13:08:02 +0330

    fix

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ ln -s ~/.dots/home-manager ~/.config/home-manager
 ln -s ~/.dots/alacritty ~/.config/alacritty
 ln -s ~/.dots/wezterm ~/.config/wezterm
 ln -s ~/.dots/fish ~/.config/fish
-ln -s ~/.dots/tmux ~/.config/tmux
 ln -s ~/.dots/nvim ~/.config/nvim
 ln -s ~/.dots/nvim-dev ~/.config/nvim-dev
 ln -s ~/.dots/ags ~/.config/ags
 ln -s ~/.dots/hypr ~/.config/hypr
-ln -s ~/.dots/river ~/.config/river
