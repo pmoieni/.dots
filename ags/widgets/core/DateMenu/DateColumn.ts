160e7b19e41a5578b5e0407f35890f1cc1d0d1eb
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2024-10-08 21:54:36 +0330

    cleanup

--- ags/widgets/core/DateMenu/DateColumn.ts
+++ ags/widgets/core/DateMenu/DateColumn.ts
@@ -2,6 +2,73 @@ import icons from "lib/icons";
 import { cpu, ram } from "lib/variables";
 import { MediaControl } from "widgets/shared/MediaControl";
 
+const Stats = () =>
+    Widget.CenterBox({
+        class_name: "stats horizontal",
+        startWidget: Widget.Box({
+            class_name: "stats-box vertical",
+            vertical: true,
+            children: [
+                Widget.Box({
+                    class_name: "progress-box",
+                    child: Widget.CircularProgress({
+                        class_name: "progress",
+                        expand: true,
+                        value: cpu.bind(),
+                        child: Widget.Icon(icons.ui.cpu),
+                    }),
+                }),
+                Widget.Box({
+                    class_name: "info-box horizontal",
+                    children: [
+                        Widget.Label({
+                            expand: true,
+                            class_name: "label",
+                            label: "CPU",
+                        }),
+                        Widget.Label({
+                            expand: true,
+                            label: cpu
+                                .bind()
+                                .as((v) => Math.ceil(v * 100).toString() + "%"),
+                        }),
+                    ],
+                }),
+            ],
+        }),
+        endWidget: Widget.Box({
+            class_name: "stats-box vertical",
+            vertical: true,
+            children: [
+                Widget.Box({
+                    class_name: "progress-box",
+                    child: Widget.CircularProgress({
+                        class_name: "progress",
+                        expand: true,
+                        value: ram.bind(),
+                        child: Widget.Icon(icons.ui.ram),
+                    }),
+                }),
+                Widget.Box({
+                    class_name: "info-box horizontal",
+                    children: [
+                        Widget.Label({
+                            expand: true,
+                            class_name: "label",
+                            label: "RAM",
+                        }),
+                        Widget.Label({
+                            expand: true,
+                            label: ram
+                                .bind()
+                                .as((v) => Math.ceil(v * 100).toString() + "%"),
+                        }),
+                    ],
+                }),
+            ],
+        }),
+    });
+
 export default () =>
     Widget.Box({
         vertical: true,
@@ -14,79 +81,7 @@ export default () =>
                     hpack: "center",
                 }),
             }),
-            Widget.CenterBox({
-                class_name: "stats horizontal",
-                startWidget: Widget.Box({
-                    class_name: "stats-box vertical",
-                    vertical: true,
-                    children: [
-                        Widget.Box({
-                            class_name: "progress-box",
-                            child: Widget.CircularProgress({
-                                class_name: "progress",
-                                expand: true,
-                                value: cpu.bind(),
-                                child: Widget.Icon(icons.ui.cpu),
-                            }),
-                        }),
-                        Widget.Box({
-                            class_name: "info-box horizontal",
-                            children: [
-                                Widget.Label({
-                                    expand: true,
-                                    class_name: "label",
-                                    label: "CPU",
-                                }),
-                                Widget.Label({
-                                    expand: true,
-                                    label: cpu
-                                        .bind()
-                                        .as(
-                                            (v) =>
-                                                Math.floor(v * 100).toString() +
-                                                "%"
-                                        ),
-                                }),
-                            ],
-                        }),
-                    ],
-                }),
-                endWidget: Widget.Box({
-                    class_name: "stats-box vertical",
-                    vertical: true,
-                    children: [
-                        Widget.Box({
-                            class_name: "progress-box",
-                            child: Widget.CircularProgress({
-                                class_name: "progress",
-                                expand: true,
-                                value: ram.bind(),
-                                child: Widget.Icon(icons.ui.ram),
-                            }),
-                        }),
-                        Widget.Box({
-                            class_name: "info-box horizontal",
-                            children: [
-                                Widget.Label({
-                                    expand: true,
-                                    class_name: "label",
-                                    label: "RAM",
-                                }),
-                                Widget.Label({
-                                    expand: true,
-                                    label: ram
-                                        .bind()
-                                        .as(
-                                            (v) =>
-                                                Math.floor(v * 100).toString() +
-                                                "%"
-                                        ),
-                                }),
-                            ],
-                        }),
-                    ],
-                }),
-            }),
             MediaControl(),
+            Stats(),
         ],
     });
