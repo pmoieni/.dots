17d46c21f29a818869b27cd7c1848b7ca68b8ad8
pmoieni <62774242+pmoieni@users.noreply.github.com>
2025-08-11 14:41:52 +0330

    fml

--- nix/home-manager/hosts/asus/home.nix
+++ nix/home-manager/hosts/asus/home.nix
@@ -3,51 +3,47 @@
 
   home.file = {
     "${config.xdg.configHome}/alacritty" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/alacritty";
-      recursive = true;
-    };
-    "${config.xdg.configHome}/home-manager" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/home-manager";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/alacritty";
       recursive = true;
     };
     "${config.xdg.configHome}/nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/nvim";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/nvim";
       recursive = true;
     };
     "${config.xdg.configHome}/nvim-dev" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/nvim-dev";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/nvim-dev";
       recursive = true;
     };
     "${config.xdg.configHome}/wezterm" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/wezterm";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/wezterm";
       recursive = true;
     };
     "${config.xdg.configHome}/zed" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/zed";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/zed";
       recursive = true;
     };
     "${config.xdg.configHome}/fish" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/fish";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/fish";
       recursive = true;
     };
     "${config.xdg.configHome}/niri" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/niri";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/niri";
       recursive = true;
     };
     "${config.xdg.configHome}/waybar" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/waybar";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/waybar";
       recursive = true;
     };
     "${config.xdg.configHome}/swaylock" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/swaylock";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/swaylock";
       recursive = true;
     };
     "${config.xdg.configHome}/mako" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/mako";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/mako";
       recursive = true;
     };
     "${config.xdg.configHome}/fuzzel" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/fuzzel";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/.config/fuzzel";
       recursive = true;
     };
   };
